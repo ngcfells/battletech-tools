6b1c1fbd920417b74548e09cc60e2669319dd544
ngcfells <87544774+ngcfells@users.noreply.github.com>
2026-09-15 16:16:11 -0600

    Update rules-level-options.ts
    Expanded to include all official and custom homebrew as levels

--- src/data/rules-level-options.ts
+++ src/data/rules-level-options.ts
@@ -2,9 +2,39 @@ import { IRulesLevelOption } from "./data-interfaces";
 
 export const btRulesLevelOptions: IRulesLevelOption[] = [
     {
+        id: 0,
+        sswid: null,
+        tag: "beginner",
+        name: "Beginners",
+    },
+    {
         id: 1,
         sswid: 0,
         tag: "intro",
         name: "Introductory",
+    },
+    {
+        id: 2,
+        sswid: null,
+        tag: "std",
+        name: "Standard",
+    },
+    {
+        id: 3,
+        sswid: null,
+        tag: "adv",
+        name: "Advanced",
+    },
+    {
+        id: 4,
+        sswid: null,
+        tag: "exp",
+        name: "Experimental",
+    },
+    {
+        id: 5,
+        sswid: null,
+        tag: "custom",
+        name: "Custom Homebrew",
     }
 ]
