9423202056dbe2803bf9dbe5fdb858297c53436f
ngcfells <87544774+ngcfells@users.noreply.github.com>
2026-09-15 16:06:55 -0600

    Update mech-type-options.ts
    Added options for Tripod, LAM, and HyVee

--- src/data/mech-type-options.ts
+++ src/data/mech-type-options.ts
@@ -13,5 +13,20 @@ export const mechTypeOptions: IMechType[] = [
 		id: 2,
 		tag: "quad",
 		name:  "Quad",
+	},
+	{
+		id: 3,
+		tag: "tripod",
+		name:  "Tripod",
+	},
+	{
+		id: 4,
+		tag: "lam",
+		name:  "LAM",
+	},
+	{
+		id: 5,
+		tag: "hyvee",
+		name:  "HyVee",
 	}
 ];
